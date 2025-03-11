3ca9865b987fd358b5ab9f083f0939312424c6c2
Shokri Francis Raoof <raoofs.de@gmail.com>
2025-03-11 09:27:51 +0100

    new logic

--- aufgabe_BONUS_1.dart
+++ aufgabe_BONUS_1.dart
@@ -1,14 +1,15 @@
 void main() {
-  String word = "rentner";  
-  bool isPalindrom = true;
-
-  for (int i = 0; i < word.length / 2; i++) { 
-    if (word[i] != word[word.length - 1 - i]) {
-      isPalindrom = false;
+  String word = "otto";  
+  bool isPalindrom() {
+    for (int i = 0, x = word.length - 1; i < x; i++, x--) {
+      if (word[i] != word[x]) {
+        return false;
+      }
     }
+    return true;
   }
 
-  if (isPalindrom) {
+  if (isPalindrom()) {
     print('$word ist ein Palindrom');
   } else {
     print('$word ist kein Palindrom');
